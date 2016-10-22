6f8e3cd0d434c471eedffb25aa46e2738874da21
camilin87 <camilin87@gmail.com>
2016-10-22 18:43:39 -0400

    start moving things off the render method

--- src/scripts/index/components/SupplementFactsContainer.jsx
+++ src/scripts/index/components/SupplementFactsContainer.jsx
@@ -2,27 +2,41 @@ import React from 'react'
 import LabelIngredient from './LabelIngredient.jsx';
 
 export default class SupplementFactsContainer extends React.Component {
-    render (){
-        var viewModel = {
-            percentOfDailyValueAdditionalSymbol: "^",
-            dailyValueIngredients: [
-                {name: "Calcium", source: "AAAA", quantity: 14, unit: "mg", percentage: "15 %"},
-                {name: "Chlorine", quantity: 14, unit: "mg", percentage: "5 %"},
-                {name: "Sodium", quantity: 14, unit: "mg"}
-            ]
-        }
 
+    constructor(props) {
+        super(props)
+
+        if (!this.props.viewModel){
+            this.state = {
+                viewModel: {
+                    percentOfDailyValueAdditionalSymbol: "^",
+                    dailyValueIngredients: [
+                        {name: "Calcium", source: "AAAA", quantity: 14, unit: "mg", percentage: "15 %"},
+                        {name: "Chlorine", quantity: 14, unit: "mg", percentage: "5 %"},
+                        {name: "Sodium", quantity: 14, unit: "mg"}
+                    ]
+                }
+            }
+        }
+        else {
+            this.state = {
+                viewModel: this.props.viewModel
+            }
+        }
+    }
+
+    render (){
         return (
             <table>
                 <thead>
                     <tr>
                         <th></th>
                         <th>Amount per Serving</th>
-                        <th>% Daily Value {viewModel.percentOfDailyValueAdditionalSymbol}</th>
+                        <th>% Daily Value {this.state.viewModel.percentOfDailyValueAdditionalSymbol}</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {viewModel.dailyValueIngredients.map(i => <LabelIngredient key={i.name} item={i}/>)}
+                    {this.state.viewModel.dailyValueIngredients.map(i => <LabelIngredient key={i.name} item={i}/>)}
                 </tbody>
             </table>
         )
