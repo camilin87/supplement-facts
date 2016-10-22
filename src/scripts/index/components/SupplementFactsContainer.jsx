44ebefd4401e680d44a6747c64d82e66c84f769d
camilin87 <camilin87@gmail.com>
2016-10-22 19:36:48 -0400

    change the test data

--- src/scripts/index/components/SupplementFactsContainer.jsx
+++ src/scripts/index/components/SupplementFactsContainer.jsx
@@ -13,8 +13,13 @@ export default class SupplementFactsContainer extends React.Component {
             vm = {
                 percentOfDailyValueAdditionalSymbol: "^=",
                 dailyValueIngredients: [
-                    {name: "Calcium", source: "AAAA", quantity: 14, unit: "mg", percentage: "15 %"},
-                    {name: "Chlorine", quantity: 14, unit: "mg", percentage: "5 %"},
+                    {name: "Vitamin A", source: "AAAA", quantity: 14, unit: "mg", percentage: "15 %"},
+                    {name: "Vitamin C", source: "BBBB", quantity: 10, unit: "mg", percentage: "5 %"},
+                    {name: "Vitamin D", source: "CCCC", quantity: 11, unit: "mg", percentage: "< 1 %"}
+                ],
+                nonDailyValueIngredients: [
+                    {name: "Calcium", source: "AAAA", quantity: 14, unit: "mg"},
+                    {name: "Chlorine", quantity: 14, unit: "mg"},
                     {name: "Sodium", quantity: 14, unit: "mg"}
                 ]
             }
