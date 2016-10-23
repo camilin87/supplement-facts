3f22d4a4304a8125cf7367eabe0272d627beff8c
camilin87 <camilin87@gmail.com>
2016-10-23 15:08:16 -0400

    try the heavy guns to prevent the stupid minor look and feel issue on the iphone

--- src/scripts/index/components/SupplementFactsContainer.jsx
+++ src/scripts/index/components/SupplementFactsContainer.jsx
@@ -69,10 +69,10 @@ export default class SupplementFactsContainer extends React.Component {
 
 
                 <div className="row">
-                  <div className="col-xs-8">
+                  <div className="col-xs-6">
                     <h3>Amount per Serving</h3>
                   </div>
-                  <div className="col-xs-4 text-right">
+                  <div className="col-xs-6 text-right">
                     <DailyValueHeader addSymbol={this.state.percentOfDailyValueAdditionalSymbol}/>
                   </div>
                 </div>
