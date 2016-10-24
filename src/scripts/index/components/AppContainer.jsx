772b05cc2f4085260b981d3bdb64e6fc730c297a
camilin87 <camilin87@gmail.com>
2016-10-24 07:43:03 -0400

    move the label to the right

--- src/scripts/index/components/AppContainer.jsx
+++ src/scripts/index/components/AppContainer.jsx
@@ -6,10 +6,10 @@ export default class AppContainer extends React.Component {
         return (
             <div className="container">
               <div className="row">
+                <div className="col-sm-6"></div>
                 <div className="col-sm-6">
                     <SupplementFactsContainer />
                 </div>
-                <div className="col-sm-6"></div>
               </div>
             </div>
         )
