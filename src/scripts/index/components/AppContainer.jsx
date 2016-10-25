323785c88b16a2935018444599b9c998e9ca1a4e
camilin87 <camilin87@gmail.com>
2016-10-25 07:56:44 -0400

    start refactoring the data input to its own component

--- src/scripts/index/components/AppContainer.jsx
+++ src/scripts/index/components/AppContainer.jsx
@@ -1,104 +1,30 @@
 import React from 'react'
 import SupplementFactsContainer from './SupplementFactsContainer.jsx'
+import SupplementFactsInput from './SupplementFactsInput.jsx'
 
 export default class AppContainer extends React.Component {
     constructor(props){
         super(props)
 
+        this.handleSupplementFactsInput = this.handleSupplementFactsInput.bind(this)
+
         this.state = {
-            labelData: this.getVm1()
+            labelData: {}
         }
     }
 
-    getVm1() {
-        return {
-            servingSizeInfo: {
-                value: 23,
-                type: "packet",
-                additionalComments: "(8g) 1 tbsp",
-                servingsPerContainer: 10
-            },
-            percentOfDailyValueAdditionalSymbol: "^",
-            disclaimers: {
-                percentOfDailyValueAdditionalSymbol: "^",
-                displayDailyValueNotEstablished: true,
-                displayChildrenDisclaimer: true,
-                displayPregnantWomenDisclaimer: true
-            },
-            otherIngredients: {
-                otherIngredients: "pb, hg",
-                allergens: "nuts, penicillin"
-            },
-            businessInfo: {
-                distributedByLabel: "Distributed by",
-                businessName: "Apple",
-                streetAddressLine1: "One infinite loop",
-                streetAddressLine2: " --- ",
-                city: "cupertino",
-                state: "CA",
-                zipCode: "55555",
-                phone: "1-800-my-apple"
-            },
-            dailyValueIngredients: [
-                {name: "Vitamin A", source: "AAAA", quantity: 14, unit: "mg", percentage: "15 %"},
-                {name: "Vitamin C", source: "BBBB", quantity: 10, unit: "mg", percentage: "5 %"},
-                {name: "Vitamin D", source: "CCCC", quantity: 11, unit: "mg", percentage: "< 1 %"}
-            ],
-            nonDailyValueIngredients: [
-                {name: "Calcium", source: "AAAA", quantity: 14, unit: "mg"},
-                {name: "Chlorine", quantity: 14, unit: "mg"},
-                {name: "Sodium", quantity: 14, unit: "mg"}
-            ]
-        }
-    }
-
-    getVm2() {
-        return {
-            servingSizeInfo: {
-                value: 1,
-                type: "bottle",
-                additionalComments: "1 tbsp",
-                servingsPerContainer: 1
-            },
-            disclaimers: {
-                displayDailyValueNotEstablished: true,
-                displayChildrenDisclaimer: false,
-                displayPregnantWomenDisclaimer: false
-            },
-            otherIngredients: {},
-            businessInfo: {},
-            dailyValueIngredients: [
-                {name: "Vitamin C", quantity: 10, unit: "mg", percentage: "5 %"},
-                {name: "Vitamin D", quantity: 11, unit: "mg", percentage: "< 1 %"}
-            ],
-            nonDailyValueIngredients: [
-                {name: "Sodium", quantity: 14, unit: "mg"}
-            ]
-        }
+    handleSupplementFactsInput(supplementFactsInput){
+        this.setState({
+            labelData: supplementFactsInput
+        })
     }
 
     render (){
-        var sender = this
-
-        function displayLabel1(){
-            sender.setState({
-                labelData: sender.getVm1()
-            })
-        }
-
-        function displayLabel2(){
-            sender.setState({
-                labelData: sender.getVm2()
-            })
-        }
-
         return (
             <div className="container">
               <div className="row">
                 <div className="col-sm-6">
-                    <button type="button" className="btn btn-primary" onClick={displayLabel1}>Label 1</button>
-                    <button type="button" className="btn btn-success" onClick={displayLabel2}>Label 2</button>
-
+                    <SupplementFactsInput onChange={this.handleSupplementFactsInput}/>
                 </div>
                 <div className="col-sm-6">
                     <SupplementFactsContainer data={this.state.labelData} />
---
+++ src/scripts/index/components/SupplementFactsInput.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+
+export default class DisplayDisclaimers extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.handleChange = this.handleChange.bind(this)
+
+        this.displayLabel1 = this.displayLabel1.bind(this)
+        this.displayLabel2 = this.displayLabel2.bind(this)
+    }
+
+    handleChange(change){
+        this.props.onChange(change)
+    }
+
+    displayLabel1(){
+        function getVm1() {
+            return {
+                servingSizeInfo: {
+                    value: 23,
+                    type: "packet",
+                    additionalComments: "(8g) 1 tbsp",
+                    servingsPerContainer: 10
+                },
+                percentOfDailyValueAdditionalSymbol: "^",
+                disclaimers: {
+                    percentOfDailyValueAdditionalSymbol: "^",
+                    displayDailyValueNotEstablished: true,
+                    displayChildrenDisclaimer: true,
+                    displayPregnantWomenDisclaimer: true
+                },
+                otherIngredients: {
+                    otherIngredients: "pb, hg",
+                    allergens: "nuts, penicillin"
+                },
+                businessInfo: {
+                    distributedByLabel: "Distributed by",
+                    businessName: "Apple",
+                    streetAddressLine1: "One infinite loop",
+                    streetAddressLine2: " --- ",
+                    city: "cupertino",
+                    state: "CA",
+                    zipCode: "55555",
+                    phone: "1-800-my-apple"
+                },
+                dailyValueIngredients: [
+                    {name: "Vitamin A", source: "AAAA", quantity: 14, unit: "mg", percentage: "15 %"},
+                    {name: "Vitamin C", source: "BBBB", quantity: 10, unit: "mg", percentage: "5 %"},
+                    {name: "Vitamin D", source: "CCCC", quantity: 11, unit: "mg", percentage: "< 1 %"}
+                ],
+                nonDailyValueIngredients: [
+                    {name: "Calcium", source: "AAAA", quantity: 14, unit: "mg"},
+                    {name: "Chlorine", quantity: 14, unit: "mg"},
+                    {name: "Sodium", quantity: 14, unit: "mg"}
+                ]
+            }
+        }
+
+        this.handleChange(getVm1())
+    }
+
+    displayLabel2(){
+        function getVm2() {
+            return {
+                servingSizeInfo: {
+                    value: 1,
+                    type: "bottle",
+                    additionalComments: "1 tbsp",
+                    servingsPerContainer: 1
+                },
+                disclaimers: {
+                    displayDailyValueNotEstablished: true,
+                    displayChildrenDisclaimer: false,
+                    displayPregnantWomenDisclaimer: false
+                },
+                otherIngredients: {},
+                businessInfo: {},
+                dailyValueIngredients: [
+                    {name: "Vitamin C", quantity: 10, unit: "mg", percentage: "5 %"},
+                    {name: "Vitamin D", quantity: 11, unit: "mg", percentage: "< 1 %"}
+                ],
+                nonDailyValueIngredients: [
+                    {name: "Sodium", quantity: 14, unit: "mg"}
+                ]
+            }
+        }
+
+        this.handleChange(getVm2())
+    }
+
+    render (){
+
+        return (
+            <div>
+                <button type="button" className="btn btn-primary" onClick={this.displayLabel1}>Label 1</button>
+                <button type="button" className="btn btn-success" onClick={this.displayLabel2}>Label 2</button>
+            </div>
+        )
+    }
+}
