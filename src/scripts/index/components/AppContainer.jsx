7dab6d8a2e4e4856657ba03f16627d8c1d963ef1
camilin87 <camilin87@gmail.com>
2016-11-06 01:04:46 -0400

    correct the call to the data transformer

--- src/scripts/index/components/AppContainer.jsx
+++ src/scripts/index/components/AppContainer.jsx
@@ -16,7 +16,7 @@ export default class AppContainer extends React.Component {
 
     handleSupplementFactsInput(supplementFactsInput){
         this.setState({
-            labelData: new DataTransformer().generateLabelData()
+            labelData: new DataTransformer().generateLabelData(supplementFactsInput)
         })
     }
 
