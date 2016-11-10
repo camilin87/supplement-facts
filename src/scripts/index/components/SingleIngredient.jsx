c73bcb0b834e953d44505e9dd8e4e4819bd3b673
camilin87 <camilin87@gmail.com>
2016-11-10 17:58:29 -0500

    right align the percentage text

--- src/scripts/index/components/SingleIngredient.jsx
+++ src/scripts/index/components/SingleIngredient.jsx
@@ -6,17 +6,20 @@ import IngredientSeparator from './IngredientSeparator.jsx';
 export default class SingleIngredient extends React.Component {
     render (){
         return (
-            <div className="row">
-              <div className="col-xs-7">
-                {this.props.item.name}
-                <IngredientSource source={this.props.item.source} />
-              </div>
-              <div className="col-xs-2">
-                {this.props.item.quantity + " " + this.props.item.unit}
-              </div>
-              <div className="col-xs-1"></div>
-              <div className="col-xs-2">
-                <IngredientPercentage percentage={this.props.item.percentage} />
+            <div>
+              <div className="row">
+                <div className="col-xs-7">
+                  {this.props.item.name}
+                  <IngredientSource source={this.props.item.source} />
+                </div>
+                <div className="col-xs-2">
+                  {this.props.item.quantity + " " + this.props.item.unit}
+                </div>
+                <div className="col-xs-1"></div>
+                <div className="col-xs-1 text-right">
+                  <IngredientPercentage percentage={this.props.item.percentage} />
+                </div>
+                <div className="col-xs-1"></div>
               </div>
 
               <IngredientSeparator isLast={this.props.item.isLast}/>
