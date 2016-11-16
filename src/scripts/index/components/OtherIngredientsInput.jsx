4738bbbf1bc9becd58159f2ff1f3750eaa4c267f
camilin87 <camilin87@gmail.com>
2016-11-16 08:24:26 -0500

    minor refactor

--- src/scripts/index/components/OtherIngredientsInput.jsx
+++ src/scripts/index/components/OtherIngredientsInput.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 export default class OtherIngredientsInput extends React.Component {
     render (){
-        var ingredients = (this.props.value || [])//.map(i => `${i.name} ${i.quantity}mg`)
+        var ingredients = this.props.value || []
 
         return (
             <ul className="list-group">
                 { 
-                    ingredients.map(i => {
-                        return <li className="list-group-item" key={i.name}>
+                    ingredients.map(i => <li className="list-group-item" key={i.name}>
                             {i.name} <span className="badge">{i.quantity} mg</span>
                         </li>
-                    }) 
+                    ) 
                 }
             </ul>
         )
