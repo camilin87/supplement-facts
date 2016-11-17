d03d1fc633c8f92192ee187e9811f1ebe79c1c6a
camilin87 <camilin87@gmail.com>
2016-11-17 07:55:56 -0500

    displays the ingredients

--- src/scripts/index/components/NonDailyValueIngredientsInput.jsx
+++ src/scripts/index/components/NonDailyValueIngredientsInput.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 export default class NonDailyValueIngredientsInput extends React.Component {
     render (){
+        var ingredients = this.props.value || []
+
         return (
             <div>
-                Enter Non-DV Ingredients
+                <ul className="list-group">
+                    { 
+                        ingredients.map((i, idx) => <li className="list-group-item" key={idx}>
+                              <span>
+                                {i.name} {i.source} <span className="badge">{i.quantity} {i.unit}</span>
+                              </span>
+
+                              <span className="pull-right">
+                                <a href="#">x</a>
+                              </span>
+                            </li>
+                        ) 
+                    }
+                </ul>
             </div>
         )
     }
--- src/scripts/index/components/NonDailyValueIngredientsInput.spec.jsx
+++ src/scripts/index/components/NonDailyValueIngredientsInput.spec.jsx
@@ -19,5 +19,22 @@ describe("NonDailyValueIngredientsInput", () => {
 
         expect(component.find("li").length).toBe(0)
     })
+
+    test("Returns one item per defined ingredient", () => {
+        var ingredients = [
+            {name: "Calcium", source: "AAAA", quantity: 14, unit: "mg"},
+            {name: "Chlorine", quantity: 14, unit: "mcg"}
+        ]
+
+        const component = shallow(
+            <NonDailyValueIngredientsInput value={ingredients} />
+        )
+
+        expect(component.find("li").map(n => n.text().replace(/\s/g, ""))).toEqual([
+            "CalciumAAAA14mgx",
+            "Chlorine14mcgx"
+        ])
+        expect(component.find("a").map(n => n.text())).toEqual(["x", "x"])
+    })
 })
 
