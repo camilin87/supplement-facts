67962f36e14d87b5474b53582ffb4f7b3f26b3ad
Camilo Sanchez <camilin87@gmail.com>
2016-11-23 12:06:46 -0500

    migrate the tests off into the business info

--- src/scripts/index/components/input/BusinessInfoInput.jsx
+++ src/scripts/index/components/input/BusinessInfoInput.jsx
@@ -3,11 +3,118 @@ import React from 'react'
 export default class BusinessInfoInput extends React.Component {
     constructor(props){
         super(props)
+
+        this.state = {
+            businessInfoDistributedByLabel: "Distributed by",
+            businessInfoBusinessName: "",
+            businessInfoStreetAddressLine1: "",
+            businessInfoStreetAddressLine2: "",
+            businessInfoCity: "",
+            businessInfoState: "",
+            businessInfoZipCode: "",
+            businessInfoPhone: ""
+        }
+
+        this._handleTextChanged = this._handleTextChanged.bind(this)
+    }
+
+    _handleTextChanged(propertyName){
+        var that = this
+
+        return (event) => {
+            var stateChange = {}
+            stateChange[propertyName] = event.target.value
+
+            if (that.props.onChange){
+                that.props.onChange(stateChange)
+            }
+        }
     }
 
     render (){
         return (
-            null
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                <h3 className="panel-title">Business Info</h3>
+                </div>
+                <div className="panel-body">
+
+                <div className="input-group">
+                    <input 
+                        name="businessInfoDistributedByLabel"
+                        className="form-control"
+                        placeholder="Distributed by Heading"
+                        type="text"
+                        value={this.state.businessInfoDistributedByLabel}
+                        onChange={this._handleTextChanged("businessInfoDistributedByLabel")} />
+
+                    <span className="input-group-addon"></span>
+
+                    <input 
+                        name="businessInfoBusinessName"
+                        className="form-control"
+                        placeholder="Business Name"
+                        type="text"
+                        value={this.state.businessInfoBusinessName}
+                        onChange={this._handleTextChanged("businessInfoBusinessName")} />
+                </div>
+
+                <input 
+                    name="businessInfoStreetAddressLine1"
+                    className="form-control"
+                    placeholder="Address"
+                    type="text"
+                    value={this.state.businessInfoStreetAddressLine1}
+                    onChange={this._handleTextChanged("businessInfoStreetAddressLine1")} />
+
+                <input 
+                    name="businessInfoStreetAddressLine2"
+                    className="form-control"
+                    placeholder=""
+                    type="text"
+                    value={this.state.businessInfoStreetAddressLine2}
+                    onChange={this._handleTextChanged("businessInfoStreetAddressLine2")} />
+
+                <div className="input-group">
+                    <input 
+                        name="businessInfoCity"
+                        className="form-control"
+                        placeholder="City"
+                        type="text"
+                        value={this.state.businessInfoCity}
+                        onChange={this._handleTextChanged("businessInfoCity")} />
+
+                    <span className="input-group-addon"></span>
+
+                    <input 
+                        name="businessInfoState"
+                        className="form-control"
+                        placeholder="State"
+                        type="text"
+                        value={this.state.businessInfoState}
+                        onChange={this._handleTextChanged("businessInfoState")} />
+
+                    <span className="input-group-addon"></span>
+
+                    <input 
+                        name="businessInfoZipCode"
+                        className="form-control"
+                        placeholder="Zip"
+                        type="text"
+                        value={this.state.businessInfoZipCode}
+                        onChange={this._handleTextChanged("businessInfoZipCode")} />
+                </div>
+
+                <input 
+                    name="businessInfoPhone"
+                    className="form-control"
+                    placeholder="Phone"
+                    type="text"
+                    value={this.state.businessInfoPhone}
+                    onChange={this._handleTextChanged("businessInfoPhone")} />
+
+                </div>
+                </div>
         )
     }
 }
--- src/scripts/index/components/input/BusinessInfoInput.spec.jsx
+++ src/scripts/index/components/input/BusinessInfoInput.spec.jsx
@@ -4,12 +4,32 @@ import BusinessInfoInput from "./BusinessInfoInput.jsx"
 
 describe("BusinessInfoInput", () => {
     var component = null
-    var lastBroadcastedState = null
+    var latestBroadcastedState = null
+    var propertyUnderTest = null
+
+    function seedStatePropertyUnderTest(value) {
+        var updatedState = {}
+        updatedState[propertyUnderTest] = value
+
+        component.setState(updatedState)
+    }
+    function controlUnderTest(){
+        return component.find(`input[name='${propertyUnderTest}']`)
+    }
+    function triggerChangeForControlUnderTest(newValue){
+        controlUnderTest().simulate("change", {target: {value: newValue}})
+    }
+    function expectValueForControlUnderTestToBe(expectation){
+        expect(controlUnderTest().props().value).toBe(expectation)
+    }
+    function expectStatePropertyUnderTestToBe(expectation){
+        expect(latestBroadcastedState[propertyUnderTest]).toBe(expectation)
+    }
 
     beforeEach(() => {
-        lastBroadcastedState = null
+        latestBroadcastedState = null
         function onChangeHandler(change){
-            lastBroadcastedState = change
+            latestBroadcastedState = change
         }
 
         component = shallow(
@@ -17,8 +37,145 @@ describe("BusinessInfoInput", () => {
         )
     })
 
-    test("Returns null when there is no ingredient info", () => {
-        expect(component.html()).toBe(null)
+    test("default state", () => {
+        expect(component.state()).toEqual({
+            businessInfoDistributedByLabel: "Distributed by",
+            businessInfoBusinessName: "",
+            businessInfoStreetAddressLine1: "",
+            businessInfoStreetAddressLine2: "",
+            businessInfoCity: "",
+            businessInfoState: "",
+            businessInfoZipCode: "",
+            businessInfoPhone: ""
+        })
+    })
+
+    describe("businessInfoDistributedByLabel", () => {
+        beforeEach(() => {
+            propertyUnderTest = "businessInfoDistributedByLabel"
+        })
+
+        test("gets displayed", () => {
+            seedStatePropertyUnderTest("distributed by")
+            expectValueForControlUnderTestToBe("distributed by")
+        })
+
+        test("gets updated", () => {
+            triggerChangeForControlUnderTest("sold by")
+            expectStatePropertyUnderTestToBe("sold by")
+        })
+    })
+
+    describe("businessInfoBusinessName", () => {
+        beforeEach(() => {
+            propertyUnderTest = "businessInfoBusinessName"
+        })
+
+        test("gets displayed", () => {
+            seedStatePropertyUnderTest("tddapps")
+            expectValueForControlUnderTestToBe("tddapps")
+        })
+
+        test("gets updated", () => {
+            triggerChangeForControlUnderTest("tdd apps")
+            expectStatePropertyUnderTestToBe("tdd apps")
+        })
+    })
+
+    describe("businessInfoStreetAddressLine1", () => {
+        beforeEach(() => {
+            propertyUnderTest = "businessInfoStreetAddressLine1"
+        })
+
+        test("gets displayed", () => {
+            seedStatePropertyUnderTest("11 nw 1 st")
+            expectValueForControlUnderTestToBe("11 nw 1 st")
+        })
+
+        test("gets updated", () => {
+            triggerChangeForControlUnderTest("11 nw 2 st")
+            expectStatePropertyUnderTestToBe("11 nw 2 st")
+        })
+    })
+
+    describe("businessInfoStreetAddressLine2", () => {
+        beforeEach(() => {
+            propertyUnderTest = "businessInfoStreetAddressLine2"
+        })
+
+        test("gets displayed", () => {
+            seedStatePropertyUnderTest("apt 5")
+            expectValueForControlUnderTestToBe("apt 5")
+        })
+
+        test("gets updated", () => {
+            triggerChangeForControlUnderTest("apt 2")
+            expectStatePropertyUnderTestToBe("apt 2")
+        })
+    })
+
+    describe("businessInfoCity", () => {
+        beforeEach(() => {
+            propertyUnderTest = "businessInfoCity"
+        })
+
+        test("gets displayed", () => {
+            seedStatePropertyUnderTest("miami")
+            expectValueForControlUnderTestToBe("miami")
+        })
+
+        test("gets updated", () => {
+            triggerChangeForControlUnderTest("new york")
+            expectStatePropertyUnderTestToBe("new york")
+        })
+    })
+
+    describe("businessInfoState", () => {
+        beforeEach(() => {
+            propertyUnderTest = "businessInfoState"
+        })
+
+        test("gets displayed", () => {
+            seedStatePropertyUnderTest("fl")
+            expectValueForControlUnderTestToBe("fl")
+        })
+
+        test("gets updated", () => {
+            triggerChangeForControlUnderTest("ny")
+            expectStatePropertyUnderTestToBe("ny")
+        })
+    })
+
+    describe("businessInfoZipCode", () => {
+        beforeEach(() => {
+            propertyUnderTest = "businessInfoZipCode"
+        })
+
+        test("gets displayed", () => {
+            seedStatePropertyUnderTest("33333")
+            expectValueForControlUnderTestToBe("33333")
+        })
+
+        test("gets updated", () => {
+            triggerChangeForControlUnderTest("55555")
+            expectStatePropertyUnderTestToBe("55555")
+        })
+    })
+
+    describe("businessInfoPhone", () => {
+        beforeEach(() => {
+            propertyUnderTest = "businessInfoPhone"
+        })
+
+        test("gets displayed", () => {
+            seedStatePropertyUnderTest("555-555-5555")
+            expectValueForControlUnderTestToBe("555-555-5555")
+        })
+
+        test("gets updated", () => {
+            triggerChangeForControlUnderTest("1-800-555-5555")
+            expectStatePropertyUnderTestToBe("1-800-555-5555")
+        })
     })
 })
 
